bcf27bd5db9381ef292804585640def2245d9e5b
Ajay Singh <ajay@mentormaya.com>
2022-12-13 23:47:28 +0545

    scale of png changed

--- assets/js/generatePDF.js
+++ assets/js/generatePDF.js
@@ -4,8 +4,8 @@ function generatePDF(){
     var opt = {
         margin:       0.7,
         filename:     'myfile.pdf',
-        image:        { type: 'jpeg', quality: 0.98 },
-        html2canvas:  { scale: 2 },
+        image:        { type: 'png', quality: 0.98 },
+        html2canvas:  { scale: 1 },
         jsPDF:        { unit: 'in', format: 'A4', orientation: 'portrait' }
     };
     html2pdf().set(opt).from(post).save();
