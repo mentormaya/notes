d3e34c3b359c3e22f095d9a9632e7635f47c3e27
Ajay Singh <ajay@mentormaya.com>
2022-12-16 13:37:42 +0545

    pdf updated

--- assets/js/generatePDF.js
+++ assets/js/generatePDF.js
@@ -1,16 +1,32 @@
-function generatePDF(){
-    console.log('Generating PDF! Please wait...');
-    const post = document.querySelector('.post');
-    var pdf = new jsPDF(); 
-    var specialElementHandlers = { 
-        '#editor': function (element, renderer) { 
-            return true; 
-        } 
-    };
-    // console.log(post.innerHTML)
-    pdf.fromHTML( post.innerHTML, 15, 15, { 
-        'width': 190, 
-            'elementHandlers': specialElementHandlers 
-    }); 
-    pdf.save('sample-page.pdf');
+const { jsPDF } = window.jspdf;
+
+// const pdf = new jsPDF();
+
+const pdf = new jsPDF({
+    unit: "mm",
+    format: "a4",
+    orientation: "portrait"
+});
+
+// var width = pdf.internal.pageSize.getWidth();
+// var height = pdf.internal.pageSize.getHeight();
+
+function generatePDF() {
+    console.log("Generating PDF! Please wait...");
+    
+    const title = document.querySelector("#core-wrapper > div > h1").innerHTML;
+    const post = document.querySelector(".post");
+
+    window.html2canvas = html2canvas;
+
+    pdf.html(post, {
+        callback: function () {
+            pdf.save(title + ".pdf");
+        },
+        x: 10,
+        y: 10,
+        autoPaging: "text",
+        image:        { type: 'jpeg', quality: 1 },
+        html2canvas:  { dpi: 200, letterRendering: true, width: 1080, height: 1920, scale: 0.3},
+    });
 }
